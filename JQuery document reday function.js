6a681077978978d02931cab3f099388320e0d591
ashrafuddin1496 <asraf1496@gmail.com>
2020-12-31 07:42:11 +0600

    jquery document ready function

---
+++ JQuery document reday function.js
@@ -0,0 +1,10 @@
+(function ($){
+    $(document).ready(function (){
+        /**
+         * Image Load
+         */
+        $('input#student_photo').change(function (){
+            alert();
+        });
+    });
+})(jQuery)
